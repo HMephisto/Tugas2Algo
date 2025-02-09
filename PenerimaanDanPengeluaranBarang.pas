c634894b34167ef54d19ab697785cf3837292411
HMephisto <hakimmajid243@gmail.com>
2025-02-09 21:45:09 +0700

    Co-authored-by: Muhamad Aditya Wicaksono <AdityaIDE@users.noreply.github.com>

--- PenerimaanDanPengeluaranBarang.pas
+++ PenerimaanDanPengeluaranBarang.pas
@@ -14,9 +14,14 @@ type
    ArrayAngkaKLR = array[1..MaksPengeluaranBarang] of integer;
    ArrayStringKLR = array[1..MaksPengeluaranBarang] of string;
 var
-   a
-
+ 
 
 begin
-
+   writeln('<< PROGRAM PENERIMAAN DAN PENGELUARAN BARANG MINIMARKET SUSAH MAJU >>');
+   textcolor(15);
+   writeln('1.PENERIMAAN BARANG');
+   writeln('2.PENGELUARAN BARANG');
+   writeln('0. Keluar');
+   write('Pilihan Anda? '); readln(MenuMetode);
 end.
+e
