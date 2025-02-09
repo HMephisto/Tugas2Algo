31b4992c6affda543807df0882d1dc56804a6107
HMephisto <hakimmajid243@gmail.com>
2025-02-09 21:09:14 +0700

    first commit

---
+++ PenerimaanDanPengeluaranBarang.pas
@@ -0,0 +1,22 @@
+program PenerimaanDanPengeluaranBarang;
+{I.S.: Pengguna memilih salah satu nomor menu}
+{F.S.: Menampilkan hasil sesuai nomor menu yang dipilih}
+
+uses crt;
+
+// Kamus Global
+const
+   MaksPenerimaanBarang = 20;
+   MaksPengeluaranBarang = 20;
+type
+   ArrayAngkaMSK = array[1..MaksPenerimaanBarang] of integer;
+   ArrayStringMSK = array[1..MaksPengeluaranBarang] of string;
+   ArrayAngkaKLR = array[1..MaksPengeluaranBarang] of integer;
+   ArrayStringKLR = array[1..MaksPengeluaranBarang] of string;
+var
+   a
+
+
+begin
+
+end.
